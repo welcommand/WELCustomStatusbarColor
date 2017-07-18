ea005dcb7ff9c820d82eee325e3b130cb8fb9af2
WELCommand <ios_programming@163.com>
2017-07-18 16:26:03 +0800

    change demo UI

--- WELCustomStatusbarColorDemo/WELCustomStatusbarColorDemo/ViewController.h
+++ WELCustomStatusbarColorDemo/WELCustomStatusbarColorDemo/ViewController.h
@@ -7,9 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-
 @interface ViewController : UIViewController
-
-
 @end
 
--- WELCustomStatusbarColorDemo/WELCustomStatusbarColorDemo/ViewController.m
+++ WELCustomStatusbarColorDemo/WELCustomStatusbarColorDemo/ViewController.m
@@ -11,12 +11,19 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *changeColorBtn;
+
 @end
 
 @implementation ViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _changeColorBtn.layer.borderColor = [UIColor blackColor].CGColor;
+    _changeColorBtn.layer.borderWidth = 1;
+}
+
 - (IBAction)changeColor:(id)sender {
-    
     UIColor *color = [UIColor colorWithRed:arc4random() % 255 /255.0
                                      green:arc4random() % 255 /255.0
                                       blue:arc4random() % 255 /255.0
